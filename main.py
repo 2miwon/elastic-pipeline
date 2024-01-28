edf7e1a883bb4cffdf1a2de4023b3ff286fe7288
2miwon <zhengsfsf@gmail.com>
2024-01-28 13:19:00 +0000

    fix

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ from downstream.raw_search import *
 from database import *
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_all_bill_metadata():
@@ -14,3 +21,9 @@ def read_all_bill_metadata():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+# @app.on_event('startup')
+# def init_data():
+#     scheduler = BackgroundScheduler()
+#     scheduler.add_job(check_list_len, 'cron', second='*/5')
+#     scheduler.start()
