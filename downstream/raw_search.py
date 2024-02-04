ab818573acf2ad640d6202ad9e77b0a0dc439e05
2miwon <zhengsfsf@gmail.com>
2024-02-04 17:47:45 +0900

    fix raw_search

--- downstream/raw_search.py
+++ downstream/raw_search.py
@@ -1,37 +1,51 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-from config import *
+# from config import *
 
-def raw_search(query: str):
+def raw_search(query: str, page:int = 0, sort:str ="RANK", searchField:str = "ALL",startDate:str = '1970.01.01', endDate:str = '2024.02.04'):
+    count = page * 10
     url = 'https://likms.assembly.go.kr/nsrch/search.do'
     data = {
-        'convert': 'fw',
-        'target': 'common',
-        'charset': 'utf-8',
+        'startCount': count,
+        'sort': sort, # RANK, DATE
+        'collection': 'bill_all',
+        'range': 'A',
+        # 'startDate': startDate,
+        # 'endDate': endDate,
+        'searchField': searchField, #ALL,
+        'reQuery': 2,
+        'realQuery': query,
         'query': query,
-        'datatype': 'json',
     }
-
+    
     response = requests.post(url, data=data)
     return response.content
 
-# /html/body/form/div/div[2]/div[2]/div[2]/div[1]
 def parse_search(byte_html: bytes):
     soup = BeautifulSoup(byte_html, 'html.parser')
     # results = [item for result in data['result'] for item in result['items']]
-    chunk = soup.select_one(RAW_SERACH_BILL_XPATH)
-    results = [ 
+    count = soup.find("p", class_="sectionResult").find("span")
+    chunk = soup.find(class_="sectionList")
+    data = [ 
         {
+            "title": li.a.text.strip(),
             "href": li.a["href"],
-            "text": li.a.text.strip()
+            "content": li.div.text.strip()
         } 
         for li in chunk.find_all("li")
     ]
-    return results
+    result = {
+        "count": count.text,
+    }
+    for i in range(len(data)):
+        result[i] = data[i]  
+    return result
 
 def get_search(query: str) -> list:
-    return parse_search(raw_search(query))
+    # return parse_search(raw_search(query))
+    # return json.loads(raw_search(query))
+    return json.dumps(parse_search(raw_search(query)))
 
 def raw_keword(query: str):
     url = 'https://likms.assembly.go.kr/nsrch/ark/ark_trans.do'
@@ -51,6 +65,9 @@ def parse_keword(byte_string: bytes):
     keywords = [item['keyword'] for result in data['result'] if result['totalcount'] for item in result['items']]
     return ','.join(keywords)
 
-def get_keword(query: str) -> list:
-    return parse_keword(raw_keword(query))
+def get_keword(query: str):
+    # return parse_keword(raw_keword(query))
+    return json.loads(raw_keword(query))
 
+# if __name__ == "__main__":
+#     print(get_search("국회"))
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from typing import Union
 from fastapi import FastAPI
-import threading
+from fastapi.responses import HTMLResponse
 from downstream.download import *
 from downstream.raw_search import *
 from database import *
+
+"""
+fastAPI
+"""
 
 app = FastAPI()
 # app.add_middleware(
@@ -18,12 +22,12 @@ app = FastAPI()
 def read_all_bill_metadata():
     return read_all_bill_metadata()
 
-@app.get("/raw_search/{query}")
-def raw_search(query: str):
+@app.get("/search/{query}")
+def search(query: str):
     return get_search(query)
 
-@app.get("/recommend_keword/{query}")
-def recommend_keword(query: str):
+@app.get("/keword/{query}")
+def keword(query: str):
     return get_keword(query)
 
 @app.get("/items/{item_id}")
@@ -34,7 +38,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def init_data():
     print("hello!")
     db_init_check()
-    loading_file()
+    # loading_file()
 #     scheduler = BackgroundScheduler()
 #     scheduler.add_job(check_list_len, 'cron', second='*/5')
 #     scheduler.start()
